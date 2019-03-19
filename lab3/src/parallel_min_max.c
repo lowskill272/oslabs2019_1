94f4a52db636b0b71828c72df34276330f19a69d
pechmar <pech.mar98@gmail.com>
2019-03-19 08:05:56 +0000

    nom2

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -18,7 +18,7 @@
 int main(int argc, char **argv) {
   int seed = -1;
   int array_size = -1;
-  int pnum = -1;
+  int pnum = -1; //число параллельных процессов
   bool with_files = false;
 
   while (true) {
@@ -40,18 +40,26 @@ int main(int argc, char **argv) {
         switch (option_index) {
           case 0:
             seed = atoi(optarg);
-            // your code here
-            // error handling
+            if (seed <= 0) {
+		          printf("seed is a positive number\n");
+		          return 1;
+	          }
+
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
+            if (array_size <= 0) {
+		         printf("array_size is a positive number\n");
+		         return 1;
+            }
+
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
+            if (pnum <= 0) {
+		         printf("pnum is a positive number\n");
+		         return 1;
+            }
             break;
           case 3:
             with_files = true;
