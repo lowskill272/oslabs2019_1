f3b2ba6c8fbff2f60272fa1342b2e06308a0bef8
pechmar <pech.mar98@gmail.com>
2019-03-19 07:13:58 +0000

    nom 1

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -4,21 +4,18 @@
 #include <limits.h>
 
 struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
-  struct MinMax min_max;
-  min_max.min = INT_MAX;
-  min_max.max = INT_MIN;
+	struct MinMax min_max;
+	min_max.min = INT_MAX;
+	min_max.max = INT_MIN;
+	min_max.min = *array;
+	min_max.max = *array;
+	for (int i = begin; i<end; i++)
+	{
+		if (*(array + i)<min_max.min)
+			min_max.min = *(array + i);
+		if (*(array + i)>min_max.max)
+			min_max.max = *(array + i);
+	}
 
-//min_max.min=array[0];
-//min_max.max=array[0];
-//printf("min: %d\n", min_max.min);
-  //printf("max: %d\n", min_max.max);
-  for (int i=begin; i<end; i++)
-  {
-      if (array[i]<min_max.min)
-        min_max.min=array[i];
-      if (array[i]>min_max.max)
-        min_max.max=array[i];
-  }
-
-  return min_max;
+	return min_max;
 }
--- lab3/src/sequential_min_max.c
+++ lab3/src/sequential_min_max.c
@@ -5,32 +5,34 @@
 #include "utils.h"
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    printf("Usage: %s seed arraysize\n", argv[0]);
-    return 1;
-  }
+	if (argc != 3) {
+		printf("Usage: %s seed arraysize\n", argv[0]);
+		return 1;
+	}
 
-  int seed = atoi(argv[1]);
-  if (seed <= 0) {
-    printf("seed is a positive number\n");
-    return 1;
-  }
+	int seed = atoi(argv[1]);
+	if (seed <= 0) {
+		printf("seed is a positive number\n");
+		return 1;
+	}
 
-  int array_size = atoi(argv[2]);
-  if (array_size <= 0) {
-    printf("array_size is a positive number\n");
-    return 1;
-  }
+	int array_size = atoi(argv[2]);
+	if (array_size <= 0) {
+		printf("array_size is a positive number\n");
+		return 1;
+	}
+	
+	int *array = (int*)malloc(array_size * sizeof(int));
+	GenerateArray(array, array_size, seed);
+	for (int i = 0; i < array_size; i++)
+		printf("%d\n", array[i]);
+	struct MinMax min_max = GetMinMax(array, 0, array_size);
+	free(array);
 
-  int *array = malloc(array_size * sizeof(int));
-  GenerateArray(array, array_size, seed);
-  struct MinMax min_max = GetMinMax(array, 0, array_size);
-  free(array);
+	printf("min: %d\n", min_max.min);
+	printf("max: %d\n", min_max.max);
 
-  //printf("min: %d\n", min_max.min);
-  //printf("max: %d\n", min_max.max);
-
-for (int i=0;i<array_size;i++)
-    printf("i: %d\n", array[i]);
-  return 0;
+	//for (int i=0;i<array_size;i++)
+		//printf(" %d\n", array[i]);
+	return 0;
 }
